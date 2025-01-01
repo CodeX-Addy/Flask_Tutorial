5d497ae154abb5efd7f27f0c5e7c9778ef24cf3b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2025-01-01 23:33:22 +0530

    add: app

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template,request
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'YOUR_DB_URI'
 
-
 db = SQLAlchemy(app)
-
 
 class Esoteric(db.Model):
     __tablename__ = 'connect'  # Set the table name explicitly
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
-
-
+    
 @app.route("/org", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -25,7 +23,6 @@ def home():
         else:
             return "Email not returned.."  # Debugging message for empty email
     return render_template("home.html")
-
 
 @app.route("/show_data")
 def show_data():
